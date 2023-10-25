770b47448c71af2def9706365e2fca190ff65df8
AnastasiyaAlisenok <strelkinz1@yandex.ru>
2023-10-25 10:27:33 +0300

    fix: rewrite search component to class component

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,15 +1,18 @@
+import React, { Component } from 'react';
 import './Search.scss';
 import searchIcon from '../../assets/search.svg';
 
-const Search = (): JSX.Element => {
-  return (
-    <form className="search">
-      <input className="search__input" type="text" />
-      <button className="search__button" aria-label="Search" type="submit">
-        <img className="search__icon" src={searchIcon} alt="search-icon" />
-      </button>
-    </form>
-  );
-};
+class Search extends Component {
+  render(): React.ReactNode {
+    return (
+      <form className="search">
+        <input className="search__input" type="text" />
+        <button className="search__button" aria-label="Search" type="submit">
+          <img className="search__icon" src={searchIcon} alt="search-icon" />
+        </button>
+      </form>
+    );
+  }
+}
 
 export default Search;
