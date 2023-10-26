aa0a7c943a55adf4e809e097f019eaa42134c5cc
AnastasiyaAlisenok <strelkinz1@yandex.ru>
2023-10-26 14:56:42 +0300

    fix: correct search styles

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -2,11 +2,20 @@ import React, { Component } from 'react';
 import './Search.scss';
 import searchIcon from '../../assets/search.svg';
 
-class Search extends Component {
+interface SearchProps {
+  value: string;
+}
+
+class Search extends Component<SearchProps> {
+  public constructor(props: SearchProps) {
+    super(props);
+  }
+
   render(): React.ReactNode {
+    const { value } = this.props;
     return (
       <form className="search">
-        <input className="search__input" type="text" />
+        <input className="search__input" type="text" value={value} />
         <button className="search__button" aria-label="Search" type="submit">
           <img className="search__icon" src={searchIcon} alt="search-icon" />
         </button>
