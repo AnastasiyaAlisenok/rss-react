bd9abdbc842da698a07c3af7ae6f50f930925d57
AnastasiyaAlisenok <strelkinz1@yandex.ru>
2023-10-26 15:32:47 +0300

    feat: add loader

--- src/components/ItemsList/ItemsList.tsx
+++ src/components/ItemsList/ItemsList.tsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import getItems from '../../api/apiRequests';
 import ItemCard from '../ItemCard/ItemCard';
 import { CharacterType } from '../../types/types';
+import Loader from '../Loader/Loader';
 
 interface ItemListType {
   value: string;
@@ -10,26 +11,32 @@ interface ItemListType {
 interface StateType {
   content: CharacterType[] | undefined;
   page: number;
+  loading: boolean;
 }
 
 class ItemsList extends Component<ItemListType> {
   state: Readonly<StateType> = {
     content: undefined,
-    page: 2,
+    page: 1,
+    loading: true,
   };
 
   componentDidMount(): void {
     const { page } = this.state;
     getItems(page).then((obj) => {
-      this.setState({ content: obj.results });
+      this.setState({ content: obj.results, loading: false });
     });
   }
 
   render(): React.ReactNode {
     const { content } = this.state;
+    const { loading } = this.state;
     return (
       <section className="list">
-        {content &&
+        {loading ? (
+          <Loader />
+        ) : (
+          content &&
           content.map((character) => (
             <ItemCard
               key={character.id}
@@ -39,7 +46,8 @@ class ItemsList extends Component<ItemListType> {
               planet={character.location.name}
               episode={character.species}
             />
-          ))}
+          ))
+        )}
       </section>
     );
   }
