cdeff86f5a469a81c5c1fed996da32623773e52a
AnastasiyaAlisenok <strelkinz1@yandex.ru>
2023-11-01 11:10:50 +0300

    fix: rewrite loader to func component

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -2,10 +2,8 @@ import React from 'react';
 import './Loader.scss';
 import loader from '../../assets/loader.gif';
 
-class Loader extends React.Component {
-  render(): React.ReactNode {
-    return <img className="loader" width="80px" src={loader} alt="loader" />;
-  }
-}
+const Loader = (): JSX.Element => {
+  return <img className="loader" width="80px" src={loader} alt="loader" />;
+};
 
 export default Loader;
