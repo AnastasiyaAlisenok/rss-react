ee0ce138e227d56fbaa5ecf7bc9daf60dfe91834
AnastasiyaAlisenok <strelkinz1@yandex.ru>
2023-11-01 11:07:23 +0300

    fix: rewrite itemcard to func component

--- src/components/ItemCard/ItemCard.tsx
+++ src/components/ItemCard/ItemCard.tsx
@@ -9,23 +9,26 @@ interface ItemCardProps {
   episode: string;
 }
 
-class ItemCard extends Component<ItemCardProps> {
-  render(): React.ReactNode {
-    const { src, name, status, planet, episode } = this.props;
-    return (
-      <div className="list__card">
-        <img className="list__card-img" src={src} alt="item-img" />
-        <div className="list__card-content">
-          <h2 className="list__card-title">{name}</h2>
-          <p className="list__card-status">{status}</p>
-          <p className="list__card-text">Species:</p>
-          <p className="list__card-description">{episode}</p>
-          <p className="list__card-text">Last known location:</p>
-          <p className="list__card-description">{planet}</p>
-        </div>
+const ItemCard: React.FC<ItemCardProps> = ({
+  src,
+  name,
+  status,
+  planet,
+  episode,
+}): JSX.Element => {
+  return (
+    <div className="list__card">
+      <img className="list__card-img" src={src} alt="item-img" />
+      <div className="list__card-content">
+        <h2 className="list__card-title">{name}</h2>
+        <p className="list__card-status">{status}</p>
+        <p className="list__card-text">Species:</p>
+        <p className="list__card-description">{episode}</p>
+        <p className="list__card-text">Last known location:</p>
+        <p className="list__card-description">{planet}</p>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default ItemCard;
